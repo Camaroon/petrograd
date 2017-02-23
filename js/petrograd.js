1deff8ceec29181bfd92ff6fbd0dc575f4a339ca
Tine De Freiesleben Røssel <SR@user-pc.kea.local>
2017-02-23 12:30:08 +0100

    load JSON produktliste and create template clone forEach

--- js/petrograd.js
+++ js/petrograd.js
@@ -2,10 +2,21 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log(sidenVises)
+
+    // læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
     visProdukt();
 }
 
-function visProdukt() {
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+
+function visProdukt(produkt) {
+    console.log(produkt);
     // klon template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     //indsæt data ind i klonen
