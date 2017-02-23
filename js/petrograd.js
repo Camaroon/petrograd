718e7cebfe1ee329c780c07ed509fed2211db3b9
Tine De Freiesleben Røssel <SR@user-pc.kea.local>
2017-02-23 12:42:11 +0100

    add data navn, pris and rabatpris

--- js/petrograd.js
+++ js/petrograd.js
@@ -20,6 +20,12 @@ function visProdukt(produkt) {
     // klon template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     //indsæt data ind i klonen
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+
+    var rabatpris = produkt.pris - (produkt.pris * produkt.rabatsats / 100);
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
 
     //append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
